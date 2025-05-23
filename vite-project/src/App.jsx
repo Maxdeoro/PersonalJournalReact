17d41e95735b9c91df6296bece90040b8437c95e
Maxdeoro <maxdeoro@gmail.com>
2025-05-23 23:55:48 +0300

    create component Button

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,9 +1,13 @@
 import './App.css';
+import Button from './components/button/Button';
 
 function App() {
 
   return (
-    <div>NEW PROJECT</div>
+    <>
+      <div>NEW PROJECT</div>
+      <Button></Button>
+    </>
   )
 }
 
