b7feea0435b2c6ac58ea19174167274492754c4a
Maxdeoro <maxdeoro@gmail.com>
2025-05-27 22:55:18 +0300

    implement useState in the Button

--- vite-project/src/components/button/Button.jsx
+++ vite-project/src/components/button/Button.jsx
@@ -1,9 +1,16 @@
 import './Button.css';
+import { useState } from 'react';
 
 function Button(children) {
+  // let text = 'SAVE';
+  const [text, setText] = useState('Save');
+  const pushTheButton = () => {
+    setText('Close');
+    console.log('Button pushed');
+  };
 
   return (
-    <button className='button accent'>Save</button>
+    <button className='button accent' onClick={pushTheButton}>{text}</button>
   )
 }
 
