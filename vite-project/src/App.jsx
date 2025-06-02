137406155923644bf37cc5f3e4384058b663465c
Maxdeoro <maxdeoro@gmail.com>
2025-06-02 23:33:05 +0300

    conditional display of items in list

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -10,53 +10,51 @@ import JournalForm from './components/journalForm/JournalForm';
 import { useState } from 'react';
 
 const INITIAL_DATA = [
-    {
-      id: 1,
-      title: 'Preparing to update courses',
-      text: 'Placerat in id cursus mi pretium tellus duis. Pretium tellus duis convallis tempus leo eu aenean.',
-      date: new Date(),
-    },
-    {
-      id: 2,
-      title: 'Hiking in the mountains',
-      text: 'Lacinia integer nunc posuere ut hendrerit semper vel.',
-      date: new Date(),
-    },
+    // {
+    //   id: 1,
+    //   title: 'Preparing to update courses',
+    //   text: 'Placerat in id cursus mi pretium tellus duis. Pretium tellus duis convallis tempus leo eu aenean.',
+    //   date: new Date(),
+    // },
+    // {
+    //   id: 2,
+    //   title: 'Hiking in the mountains',
+    //   text: 'Lacinia integer nunc posuere ut hendrerit semper vel.',
+    //   date: new Date(),
+    // },
 ];
 
 function App() {
 
   const [items, setItems] = useState(INITIAL_DATA);
 
+  // const addItem = (item) => {
+  //   setItems(oldItems => [...oldItems, {
+  //     text: item.text,
+  //     title: item.title,
+  //     date: new Date(item.date),
+  //     id: Math.max(...oldItems.map(i => i.id)) + 1,   // find max id and add 1 to create new id
+  //   }]);
+  // };
+
   const addItem = (item) => {
-    setItems(oldItems => [...oldItems, {
+  setItems(oldItems => {
+    const newId = oldItems.length === 0 ? 1 : Math.max(...oldItems.map(i => i.id)) + 1;
+    return [...oldItems, {
       text: item.text,
       title: item.title,
       date: new Date(item.date),
-      id: Math.max(...oldItems.map(item => item.id)) + 1,   // find max id and add 1 to create new id
-    }]);
-  };
-
-  const sortItems = (a, b) => {
-    if (a.date < b.date) {
-      return 1;
-    } else {
-      return -1;
-    }
-  };
+      id: newId,
+    }];
+  });
+};
 
   return (
     <div className='app'>
       <LeftPanel>
         <Header/>
         <NewPostButton/>
-        <JournalList>
-          {items.sort(sortItems).map(el => (
-            <CardButton key={el.id}>
-              <JournalItem title={el.title} text={el.text} date={el.date} />
-            </CardButton>
-          ))}
-        </JournalList>
+        <JournalList items={items}/>
       </LeftPanel>
       <Body>
         <JournalForm onSubmit={addItem}/>
--- vite-project/src/components/journalList/JournalList.jsx
+++ vite-project/src/components/journalList/JournalList.jsx
@@ -1,8 +1,27 @@
 import './JournalList.css';
+import CardButton from '../cardButton/CardButton';
+import JournalItem from '../journalItem/JournalItem';
 
-function JournalList( {children} ) {
+function JournalList( {items} ) {
+    if(items.length === 0) {
+        return <p>There is no posts yet, write a new post.</p>;
+    }
+
+    const sortItems = (a, b) => {
+        if(a.date < b.date) {
+            return 1;
+        }
+        return -1;
+    };
+
     return (
-        <div className='journal-list'>{children}</div>
+        <>
+            {items.sort(sortItems).map(el => (
+                <CardButton key={el.id}>
+                    <JournalItem title={el.title} date={el.date} text={el.text}/>
+                </CardButton>
+            ))}
+        </>
     );
 };
 
